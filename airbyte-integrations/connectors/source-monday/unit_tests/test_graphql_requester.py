db8e8df24f478f896e4e85ee14f08b7916c7f495
Christo <christo.grab@gmail.com>
2023-12-04 15:33:08 -0500

    increase test coverage to > 80%

--- airbyte-integrations/connectors/source-monday/unit_tests/test_graphql_requester.py
+++ airbyte-integrations/connectors/source-monday/unit_tests/test_graphql_requester.py
@@ -7,6 +7,8 @@ from unittest.mock import MagicMock
 import pytest
 from airbyte_cdk.sources.declarative.requesters.requester import HttpMethod
 from source_monday import MondayGraphqlRequester
+from airbyte_cdk.sources.declarative.schema.json_file_schema_loader import JsonFileSchemaLoader
+from airbyte_cdk.sources.declarative.interpolation.interpolated_string import InterpolatedString
 
 nested_object_schema = {
     "root": {
@@ -89,3 +91,48 @@ def test_get_request_params(mocker, input_schema, graphql_query, stream_name, co
         config=config,
     )
     assert requester.get_request_params(stream_state={}, stream_slice={}, next_page_token=next_page_token) == graphql_query
+
+
+@pytest.fixture
+def monday_requester():
+    return MondayGraphqlRequester(
+        name="a name",
+        url_base="https://api.monday.com/v2",
+        path="a-path",
+        config={},
+        parameters={"name": "activity_logs"},
+        limit=InterpolatedString.create("100", parameters={"name": "activity_logs"}),
+        nested_limit=InterpolatedString.create("100", parameters={"name": "activity_logs"}),
+    )
+
+def test_get_schema_root_properties(mocker, monday_requester):
+    mock_schema = {
+        "properties": {
+            "updated_at_int": {"type": "integer"},
+            "created_at_int": {"type": "integer"},
+            "pulse_id": {"type": "integer"},
+            "board_id": {"type": "integer"},
+            "other_field": {"type": "string"},
+            "yet_another_field": {"type": "boolean"}
+        }
+    }
+
+    mocker.patch.object(JsonFileSchemaLoader, 'get_json_schema', return_value=mock_schema)
+    requester = monday_requester
+    result_schema = requester._get_schema_root_properties()
+
+    assert result_schema == { 
+        "other_field": { "type": "string" },
+        "yet_another_field": { "type": "boolean" }
+    }
+
+
+def test_build_activity_query(mocker, monday_requester):
+
+    mock_stream_state = { "updated_at_int": 1636738688 }
+    object_arguments = { "stream_state": mock_stream_state }
+    mocker.patch.object(MondayGraphqlRequester, '_get_object_arguments', return_value="stream_state:{{ stream_state['updated_at_int'] }}")
+    requester = monday_requester
+
+    result = requester._build_activity_query(object_name="activity_logs", field_schema={}, sub_page=None, **object_arguments)
+    assert result == "boards(stream_state:{{ stream_state['updated_at_int'] }}){activity_logs(stream_state:{{ stream_state['updated_at_int'] }}){}}"
