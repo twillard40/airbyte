7b4dfeada44175c60bc2519fbc7470d612133f8c
nguyenaiden <duy@airbyte.io>
2024-01-10 13:17:57 -0800

    Updated query to use TOP instead of LIMIT

--- airbyte-integrations/connectors/source-mssql/src/main/java/io/airbyte/integrations/source/mssql/initialsync/MssqlInitialLoadRecordIterator.java
+++ airbyte-integrations/connectors/source-mssql/src/main/java/io/airbyte/integrations/source/mssql/initialsync/MssqlInitialLoadRecordIterator.java
@@ -121,7 +121,7 @@ public class MssqlInitialLoadRecordIterator extends AbstractIterator<JsonNode>
         if (isCompositeKeyLoad) {
           sql = "SELECT %s FROM %s ORDER BY %s".formatted(wrappedColumnNames, fullTableName, quotedCursorField);
         } else {
-          sql = "SELECT %s FROM %s ORDER BY %s LIMIT %s".formatted(wrappedColumnNames, fullTableName, quotedCursorField, chunkSize);
+          sql = "SELECT TOP %s %s FROM %s ORDER BY %s".formatted(chunkSize, wrappedColumnNames, fullTableName, quotedCursorField);
         }
         final PreparedStatement preparedStatement = connection.prepareStatement(sql);
         LOGGER.info("Executing query for table {}: {}", tableName, preparedStatement);
@@ -139,8 +139,8 @@ public class MssqlInitialLoadRecordIterator extends AbstractIterator<JsonNode>
           // can just issue a query
           // without any chunking.
           if (ocInfo.ocMaxValue() != null) {
-            sql = "SELECT %s FROM %s WHERE %s > ? AND %s <= ? ORDER BY %s LIMIT %s".formatted(wrappedColumnNames, fullTableName,
-                quotedCursorField, quotedCursorField, quotedCursorField, chunkSize);
+            sql = "SELECT TOP %s %s FROM %s WHERE %s > ? AND %s <= ? ORDER BY %s".formatted(chunkSize, wrappedColumnNames, fullTableName,
+                quotedCursorField, quotedCursorField, quotedCursorField);
           } else {
             sql = "SELECT %s FROM %s WHERE %s > ? ORDER BY %s".formatted(wrappedColumnNames, fullTableName,
                 quotedCursorField, quotedCursorField);
